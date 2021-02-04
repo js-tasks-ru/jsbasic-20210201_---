8e151bf780ad0442b105571fddbc382898c78427
NK <nikita.zyamzin@yandex.ru>
2021-02-04 17:11:48 +0300

    Completed 1-module\ 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,10 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+	let result = n;
+	let i = result <= 1 ? result = 1 : result-1;
+	for (i; i > 0; i--) {
+		result = result * i;
+	}
+return result;
 }
