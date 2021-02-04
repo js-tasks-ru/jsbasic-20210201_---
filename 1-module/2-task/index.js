84bd47c498b6a20b2ebe3fd100142cb75ea8197e
NK <nikita.zyamzin@yandex.ru>
2021-02-04 17:52:43 +0300

    Completed 1-module\ 2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,11 +12,30 @@ function print(text) {
  * @returns {boolean}
  */
 function isValid(name) {
-  // ваш код...
+	if (name !== null) {
+	  if (!name.match(/\s/g)) {
+		  if (name.length >= 4) {
+			if (isNaN(name)) {
+			  // console.log("typeof String");
+			  return true;					
+			} else if (isFinite(name)) {
+			  // console.log("typeof Number");
+			  return false;
+			}
+		  }
+		  else if (name.length < 4) {
+		  // console.log("Need more letters");
+		  return false;
+		  }
+	  } else {
+		// console.log("Find space");
+		return false;
+	  }
+	} else return false;
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt('Введите ваше имя', 'UserName');
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
