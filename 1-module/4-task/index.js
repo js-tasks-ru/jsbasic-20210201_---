9c5e78aab5452b82b200f5fe1b196b9c50fddf29
NK <nikita.zyamzin@yandex.ru>
2021-02-04 17:59:25 +0300

    Completed 1-module\ 4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,6 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+	let arg_1 = "1xbet", arg_2 = "xxx";
+	return str.toLowerCase().includes(arg_1) || str.toLowerCase().includes(arg_2);
 }
