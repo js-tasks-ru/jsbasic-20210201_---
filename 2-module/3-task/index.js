f52f228c76e67a48347701cc52250d7f7c03bc88
NK <nikita.zyamzin@yandex.ru>
2021-02-08 17:30:00 +0300

    Completed 2-module\ 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read : function(a, b) {
+	this.property_a = a;
+	this.property_b = b;
+  },
+  sum : function(val){
+	return this.property_a + this.property_b
+  },
+  mul : function(val){
+	return this.property_a * this.property_b
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
