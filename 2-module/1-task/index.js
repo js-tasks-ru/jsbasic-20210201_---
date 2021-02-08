b2ebfb8ffe8dd575515d83e7aa486cc180f24e33
NK <nikita.zyamzin@yandex.ru>
2021-02-08 18:00:10 +0300

    Completed 2-module\ 1-task (upd)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+	let result = Number(0);
+	for (let key in salaries) {
+		if ((typeof salaries[key] !== typeof Boolean()) && (typeof salaries[key] !== typeof String())) {
+			if (!isNaN(salaries[key]) && isFinite(salaries[key])) {
+				//console.log(salaries[key]);
+				result += salaries[key];
+			}
+		}
+	}
+	return result;
 }
