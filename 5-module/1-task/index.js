b7795c3bd75895744dcb26cee7c6fad0567d0b04
NK <nikita.zyamzin@yandex.ru>
2021-02-25 19:56:49 +0300

    Completed 5-module

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,6 @@
 function hideSelf() {
-  // ваш код...
+  const button = document.querySelector('.hide-self-button');
+  button.addEventListener("click", event => {
+    button.setAttribute('hidden', true);
+  }, false);
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
-  // ваш код...
+  const getOffset = document.querySelector('div.carousel__slide').offsetWidth;
+
+  const carouselInner = document.querySelector('div.carousel__inner');
+  const carouselArrowRight = document.querySelector('div.carousel__arrow_right');
+  const carouselArrowLeft = document.querySelector('div.carousel__arrow_left');
+  carouselArrowLeft.style = 'display: none';
+
+  const getMaxOffset = -getOffset * (document.querySelectorAll('div.carousel__slide').length-1);
+  let currentValue = 0;
+  carouselArrowRight.addEventListener('click', event => {
+    currentValue -= getOffset;
+    carouselInner.style.transform = `translateX(${currentValue}px)`;
+    carouselArrowLeft.style = 'display';
+    if (currentValue == getMaxOffset) {
+      carouselArrowRight.style = 'display:none';
+    }
+  }, false);
+
+  carouselArrowLeft.addEventListener('click', event => {
+    currentValue += getOffset;
+    carouselInner.style.transform = `translateX(${currentValue}px)`;
+    carouselArrowRight.style = 'display';
+    if (currentValue == 0) {
+      carouselArrowLeft.style = 'display:none';
+    }
+  }, false);
 }
